224847064d325b72b00bc99dee567a0711704bac
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2018-10-01 22:11:42 -0500

    docker-compose changes with a working copying of services registering to eureka container

--- eventsubscriber/src/main/java/com/sskez/core/event/read/EventsubscriberApplication.java
+++ eventsubscriber/src/main/java/com/sskez/core/event/read/EventsubscriberApplication.java
@@ -2,11 +2,47 @@ package com.sskez.core.event.read;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
 public class EventsubscriberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventsubscriberApplication.class, args);
+    }
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Event Subscriber API",
+                "Atomic read operation for Events",
+                "1.0",
+                "MIT",
+                new Contact("Sasikumar Sugumar","http://sasikumarsugumar.io/","sasisugumar@gmail.com"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
     }
 }
